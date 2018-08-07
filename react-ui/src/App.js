a7146bfcebbec92f35bec1b89a2b45e5712739ae
Eugene Sokolyansky <evgenys@123software.ru>
2018-08-07 11:29:45 +0300

    test push heroku

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <div className="heroku-test">Я ТЕБЕ ПОРВУ СКАТЫНА!!!</div>
         <ComponentA />
         <ComponentB />
             </div>
