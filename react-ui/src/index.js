13fa08592a023caa0ab72482e04a70d76745a3e6
Andrey Barchuk <mazafakand@gmail.com>
2018-09-05 11:00:52 +0300

    READY TO START

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,30 +2,11 @@ import "babel-polyfill";
 import React,{Component}  from  'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import App from './App.js';
+import App from './containers/App.jsx';
 import '../public/favicon.ico';
 
 import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers/rootReducer'
-import createSagaMiddleware from 'redux-saga';
-
-
-import rootSaga from './sagas/rootSagas';
-
-const sagaMiddleware = createSagaMiddleware();
-
-const middleWare = [logger, sagaMiddleware]
-
-const store = createStore(
-    rootReducer,
-  {},
-  composeWithDevTools(applyMiddleware(...middleWare)),
-)
-sagaMiddleware.run(rootSaga);
+import store from './redux/store.js';
 
 ReactDom.render(
     <Provider store = {store}><App /></Provider>,
