a9d4d600a80eaff95d1955472d907e7edb8867a1
Eugene Sokolyansky <evgenys@123software.ru>
2018-11-23 15:13:47 +0200

    refact

--- react-ui/src/component/ProductItem/productItem.js
+++ react-ui/src/component/ProductItem/productItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './productItem.css';
 
 const productItem = (props) => {
-    console.log(props)
     return (
         <div className={`product-item__wrapper ${props.grid}`}>
             <img className="product-item__img" src="https://images.unsplash.com/photo-1538911467261-618f6a74b3d2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=708b47ed132c04d43e674b09c53694ee&auto=format&fit=crop&w=634&q=80" />
